440bb9ca7defec42fa5d9b30d54b38e326cf59dd
RianFriedrich <125458446+RianFriedrich@users.noreply.github.com>
2024-08-12 10:50:24 -0300

    Add files via upload

---
+++ JogoDaVelha/src/main/java/com/mycompany/jogodavelha/JogoDaVelha.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.jogodavelha;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class JogoDaVelha {
+
+    public static void main(String[] args) {
+    //   Jogador1 j1 = new Jogador1();
+
+    }
+}
